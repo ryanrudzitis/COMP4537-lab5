ca9e378c2f753f9f2e9ec4159134710e9c1e0fb8
Noorarjun Sidhu <nsidhu73@my.bcit.ca>
2023-10-12 22:57:57 -0700

    post should be working

--- app.js
+++ app.js
@@ -3,16 +3,18 @@ const url = require('url');
 const utils = require('./modules/utils');
 
 const rootEndPoint = '/COMP4537/labs/5/api/v1/sql/';
+const patientTableStatement = 'CREATE TABLE IF NOT EXISTS Patient (PatientID INT NOT NULL AUTO_INCREMENT, Name VARCHAR(100) NOT NULL, DateOfBirth DATETIME NOT NULL, PRIMARY KEY(PatientID))';
 
 http.createServer((req, res) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'application/json');
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST');
+
     const parsedUrl = url.parse(req.url, true);
     const pathName = parsedUrl.pathname;
 
     if (req.method === 'GET' && pathName === rootEndPoint) {
-        const query = parsedUrl.query;
-        const mysql = utils.mysqlConnect();
-
-        
 
     } else if (req.method === 'POST' && pathName === rootEndPoint) {
         let body = '';
@@ -26,16 +28,24 @@ http.createServer((req, res) => {
         req.on('end', () => {
             const parsedBody = url.parse(body, true);
             const query = parsedBody.query;
+
+            if (!query.dbquery) {
+                res.statusCode = 400;
+                res.end(JSON.stringify({ message: 'Missing dbquery parameter' }));
+                return;
+            }
+
             const dbquery = query.dbquery;
 
-            const mysql = utils.mysqlConnect();
-            mysql.connect(function(err) {
-                if (err) throw err;
-                mysql.query(dbquery, function(err, result) {
-                    if (err) throw err;
-                    console.log("Aight");
-                })
-            })
+            if (utils.invalidSqlAction(dbquery, 'SELECT')) {
+                res.statusCode = 400;
+                res.end(JSON.stringify({ message: 'Post request cannot contain DROP, DELETE, UPDATE, or SELECT statements' }));
+                return;
+            }
+
+            const mysql = utils.mysqlConnection();
+
+            utils.mysqlExecStatement(mysql, patientTableStatement, dbquery, res);
         });
     }
 
--- modules/utils.js
+++ modules/utils.js
@@ -1,7 +1,7 @@
 const mysql = require('mysql');
 
 module.exports = {
-    mysqlConnect: function() {
+    mysqlConnection: function () {
         return mysql.createConnection({
             host: 'localhost',
             user: 'root',
@@ -9,5 +9,49 @@ module.exports = {
             database: 'defaultdb',
             port: '3333'
         });
+    },
+
+    mysqlExecStatement: function (mysql, patientTableStatement, dbquery, res) {
+        mysql.connect(err => {
+            if (err) {
+                res.statusCode = 500;
+                res.end(JSON.stringify({ message: 'Internal server error' }));
+                return;
+            }
+
+            mysql.query(patientTableStatement, (err) => {
+                if (err) {
+                    res.statusCode = 400;
+                    res.end(JSON.stringify({ message: 'Invalid SQL statement' }));
+                    return;
+                }
+
+                mysql.query(dbquery, (err) => {
+                    if (err) {
+                        res.statusCode = 400;
+                        res.end(JSON.stringify({ message: 'Invalid SQL statement' }));
+                        return;
+                    }
+    
+                    res.end(JSON.stringify({ message: 'Statement executed successfully' }));
+                    mysql.end();
+                    return;
+                });
+            });
+        });
+    },
+
+    invalidSqlAction: function (dbquery, extraSqlAction) {
+        let sqlStatements = dbquery.split(';');
+
+        for (let i = 0; i < sqlStatements.length; i++) {
+            let sqlAction = sqlStatements[i].split(' ')[0].trim();
+            sqlAction = sqlAction.toUpperCase();
+            if (sqlAction === 'DROP' || sqlAction === 'DELETE' || sqlAction === 'UPDATE' || sqlAction === extraSqlAction) {
+                return true;
+            }
+        }
+
+        return false;
     }
 };
